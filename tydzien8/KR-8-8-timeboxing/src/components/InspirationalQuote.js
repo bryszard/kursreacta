4db9d3ff0343f7c857b97544f8d01574fbe6c82f
Piotr Brych <piotr.brych@globalapptesting.com>
2019-08-28 07:49:53 +0200

    Do homework for KR-8-8

---
+++ tydzien8/KR-8-8-timeboxing/src/components/InspirationalQuote.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const InspirationalQuote = () => {
+  const [quote, setQuote] = useState();
+
+  useEffect(() => {
+    import("inspirational-quotes")
+      .then(Quotes => {
+        setQuote(Quotes.getQuote());
+      })
+      .catch(() => console.log("Couldn't load quotes"));
+  });
+
+  return (
+    <>
+      {quote ? (
+        <figure>
+          <blockquote>{quote.text}</blockquote>
+          <figcaption>
+            <cite>{quote.author}</cite>
+          </figcaption>
+        </figure>
+      ) : (
+        "..."
+      )}
+    </>
+  );
+};
+
+export default InspirationalQuote;
