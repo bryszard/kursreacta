92540111f138125c1c7acde784a9f7daa1bfa1fc
Piotr Brych <piotr.brych@globalapptesting.com>
2019-08-30 07:49:00 +0200

    Do 1 task from homework for 8-9

---
+++ tydzien8/KR-8-9-timeboxing/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+
+function LoginForm({ errorMessage, onLoginAttempt }) {
+  const emailInput = useRef();
+  const passwordInput = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onLoginAttempt({
+      email: emailInput.current.value,
+      password: passwordInput.current.value
+    });
+    emailInput.current.value = "";
+    passwordInput.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="LoginForm">
+      {errorMessage ? (
+        <div className="LoginForm__error-message">{errorMessage}</div>
+      ) : null}
+      <label>
+        Email
+        <input ref={emailInput} type="text" defaultValue="bob@example.com" />
+      </label>
+      <br />
+      <label>
+        Hasło
+        <input ref={passwordInput} type="password" defaultValue="secret" />
+      </label>
+      <br />
+      <button>Zaloguj się</button>
+    </form>
+  );
+}
+
+export default LoginForm;
